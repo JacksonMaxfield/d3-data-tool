a4d6b67dcd215242aa9bb1f3c5084354176d855a
JacksonMaxfield <jmax825@uw.edu>
2016-04-29 19:56:39 -0700

    Update main-d3.js

--- js/main-d3.js
+++ js/main-d3.js
@@ -1,5 +1,5 @@
 $(function() {
-  d3.csv("../resources/us-cancer.csv", function(error, allData) {
+  d3.csv("/resources/us-cancer.csv", function(error, allData) {
     if (error) throw error;
 
     var xScale;
